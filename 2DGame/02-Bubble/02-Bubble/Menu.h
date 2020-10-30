b7a0cd19cc37a9c730539f5f1385d86ff0f318bb
Jaume Riera Caba <33702960+Cabahamaru@users.noreply.github.com>
2020-10-30 01:37:31 +0100

    Class Menu

---
+++ 2DGame/02-Bubble/02-Bubble/Menu.h
@@ -0,0 +1,41 @@
+#ifndef _MENU_INCLUDE
+#define _MENU_INCLUDE
+
+#include <glm/glm.hpp>
+#include "ShaderProgram.h"
+#include "Texture.h"
+#include "TexturedQuad.h"
+//#include "Text.h"
+
+#define CAMERA_WIDTH 640
+#define CAMERA_HEIGHT 480
+
+class Menu
+{
+
+public:
+	Menu();
+	~Menu();
+
+	void init();
+	void update(int deltaTime);
+	void render();
+	bool jugar();
+private:
+	void initShaders();
+	void nextLevel();
+
+private:
+	int accion;
+	//Text texto;
+	Texture imgFondo, imgCursor;
+	TexturedQuad* fondo;
+	ShaderProgram texProgram;
+	float currentTime;
+	glm::mat4 projection;
+
+};
+
+
+#endif // _MENU_INCLUDE
+
